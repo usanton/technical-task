30dd1bea64879d0842c3f83b985e97d25bf2b89c
Anton Usik <moustacheby@gmail.com>
2021-09-07 20:02:48 +0300

    Added models and ExcelReader

---
+++ diploma-practice/src/main/java/model/api/responses/ProjectResponse.java
@@ -0,0 +1,9 @@
+package model.api.responses;
+
+import lombok.Data;
+import model.api.responses.results.ProjectResult;
+
+@Data
+public class ProjectResponse extends BaseResponse<ProjectResult> {
+
+}
---
+++ diploma-practice/src/main/java/model/api/responses/SuiteResponse.java
@@ -0,0 +1,10 @@
+package model.api.responses;
+
+import lombok.Data;
+import model.api.responses.results.SuiteResult;
+
+
+@Data
+public class SuiteResponse extends BaseResponse<SuiteResult> {
+
+}
